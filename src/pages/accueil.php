ed507ba9a9240677c36f0d2dc97e7c916a747c56
lucadsv <lukds.dasilva@gmail.com>
2025-11-04 13:09:51 +0100

    Ajout de la page accueil.php dans le dossier pages

---
+++ src/pages/accueil.php
@@ -0,0 +1,45 @@
+<?php
+include("../templates/header.html");
+
+echo"
+<title>Accueil</title>
+</head>
+<body>";
+include("../templates/barnav.html");
+echo "
+<div class='video-carousel'>
+    <button class='carousel-btn left'>&lt;</button>
+
+    <div class='carousel-track'>
+        <video class='carousel-item left' autoplay loop muted playsinline>
+            <source src='../images/video2.mp4' type='video/mp4'>
+            Votre navigateur ne prend pas en charge cette vidéo.
+        </video>
+        <video class='carousel-item center' autoplay loop muted playsinline>
+            <source src='../images/video1.mov' type='video/mp4'>
+            Votre navigateur ne prend pas en charge cette vidéo.
+        </video>
+        <video class='carousel-item right' autoplay loop muted playsinline>
+            <source src='../images/video3.mp4' type='video/mp4'>
+            Votre navigateur ne prend pas en charge cette vidéo.
+        </video>
+    </div>
+
+    <button class='carousel-btn right'>&gt;</button>
+</div>
+
+<div class='TexteExplicatif'>
+    <h1>Bienvenue sur NOM DU SITE, la plateforme de calcul distribué</h1>
+    <p>C'est plus précisement une application web conçue pour exploiter un cluster Raspberry et réaliser des calculs distribués ou parallèles de manière simple et efficace. Elle permet aux utilisateurs inscrits de lancer des programmes de calcul et d’obtenir des résultats directement dans leur interface web.</p> 
+    <p>Grâce à NOM DU SITE, vous pouvez :</p> 
+    <ul>
+       <li><strong>Lancer des programmes de calcul</strong> : Utilisez nos modules pour exécuter des calculs sur le cluster Raspberry.</li>
+       <li><strong>Gérer votre profil</strong> : Créez un compte, modifiez vos informations et accédez à votre tableau de bord personnalisé.</li>
+       <li><strong>Suivre vos résultats</strong> : Visualisez les résultats de vos calculs directement sur la plateforme.</li>
+    </ul>
+    <p>La plateforme est conçue pour être accessible tant aux étudiants qu’aux professeurs souhaitant expérimenter le calcul distribué ou parallèle. Vous trouverez différents modules adaptés à vos besoins et pourrez facilement explorer les possibilités offertes par le cluster Raspberry.</p>
+    <p>Nous vous invitons à vous inscrire pour profiter pleinement de toutes les fonctionnalités et commencer à lancer vos programmes de calcul sur le cluster.</p>
+    <p>Explorez, testez et optimisez vos calculs !<br>(L'équipe de développement)</p>
+</div>
+";
+include("../templates/footer.html");
